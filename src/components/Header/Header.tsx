f49bf0ef923fd6ff27cf6a5e0129cb4a1c08b588
Parveeezy <parveeezy@gmail.com>
2022-09-07 22:56:19 +0300

    import/export

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News</a>
+            <a href="#">Messages</a>
+        </div>
+    )
+
+}
+
+export default Header;
